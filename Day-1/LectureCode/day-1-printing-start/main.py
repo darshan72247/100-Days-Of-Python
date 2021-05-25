bf578926401cf1fd2fc6ebc0d6a084eb1f613850
Darshan Patel <66130004+darshan72247@users.noreply.github.com>
2021-05-25 13:20:06 -0400

    Day 1 Complete

---
+++ Day-1/LectureCode/day-1-printing-start/main.py
@@ -0,0 +1,15 @@
+# Write your code below this line 👇
+print("Hello World")
+
+# \n - is used for new line
+print("Hello World\nHello World\nHello World")
+
+# String Concatination
+print("Hello " + "Darshan!")
+
+# Input Function
+input("What is your Name?")
+print("Hello " + input("What is your name?") + "!")
+
+# String Length Function
+print(len(input("Whats you last name?")))
---
+++ Day-1/LectureCode/day-1-variables-start/main.py
@@ -0,0 +1,18 @@
+# Variables 
+name = input("What is your name?")
+print(name)
+
+# Overridding the data in a variable
+
+number1 = 100
+# Below line would have outputted 100
+print(number1)
+# changinf values in variable
+number1 = 200
+# Now line below would output 200
+print(number1)
+
+# Other way to solve the previous challenge
+name = input("What is your name? ")
+length = len(name)
+print(length)
