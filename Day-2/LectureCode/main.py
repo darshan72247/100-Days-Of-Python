4ffdd51db94fce2ef0e072370e6da8c27d597468
Darshan Patel <66130004+darshan72247@users.noreply.github.com>
2021-05-26 19:36:51 -0400

    Day-2 Complete

---
+++ Day-2/LectureCode/main.py
@@ -0,0 +1,36 @@
+#Data Types
+
+#String
+print("Hello"[-2])
+
+#Integer
+
+print(123+456)
+
+# _ is used for our convenience for better readability
+123_456_789
+
+#Float
+3.14159
+
+#round
+print(round(5/2)) # this round the decimal 
+print(round(5/2,2)) # this round to 2 point decimal
+
+#Boolean
+
+True
+False
+
+3+5
+8-7
+8*2
+# division always give answer in float
+6/3 
+2**3
+
+# PEMDAS
+()
+**
+* /
++ -
