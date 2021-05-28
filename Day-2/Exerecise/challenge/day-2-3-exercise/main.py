1d026ac7351a5f1b4ef06d7b8e7cf2c691fb815d
Darshan Patel <66130004+darshan72247@users.noreply.github.com>
2021-05-28 01:37:16 -0400

    Day-2 exercise add

---
+++ Day-2/Exerecise/challenge/day-2-3-exercise/main.py
@@ -0,0 +1,17 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+years = 90 - int(age)
+z = years * 12
+y = years * 54
+x = years * 365
+print(f"You have {x} days, {y} weeks, and {z} months left.")
+
+
+
+
+
+
