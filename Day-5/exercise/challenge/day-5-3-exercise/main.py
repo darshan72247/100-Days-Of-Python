dc663cde26888152e90fcd2457bd83e1db24bf40
Darshan Patel <66130004+darshan72247@users.noreply.github.com>
2021-06-01 16:33:32 -0400

    Day-5 Complete

---
+++ Day-5/exercise/challenge/day-5-3-exercise/main.py
@@ -0,0 +1,8 @@
+#Write your code below this row 👇
+
+total = 0
+
+for i in range(0,101,2):
+  total += i
+
+print(total)
---
+++ Day-5/lectureCode/day-5-start/main.py
@@ -0,0 +1,16 @@
+# For loop
+
+fruits = ["Apple","Grapes","Oranges","Watermelon"]
+
+for fruit in fruits:
+  print(fruit)
+
+# Range
+
+for i in range(0,10):
+  print(i)
+
+# Range
+
+for i in range(0,10,2):
+  print(i)
