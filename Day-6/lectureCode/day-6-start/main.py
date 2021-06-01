407111e445cf5eb945b1e2b444d20f763cb4d69e
Darshan Patel <66130004+darshan72247@users.noreply.github.com>
2021-06-01 18:51:49 -0400

    Day-6 Complete

---
+++ Day-6/lectureCode/day-6-start/main.py
@@ -0,0 +1,21 @@
+# Print function
+print("Darshan")
+num_char = len("Darshan")
+print(num_char)
+
+# Function in python
+
+def welcome():
+  print("Welcome to the world of python function programming")
+
+welcome()
+
+# https://reeborg.ca/
+
+# Hurdle 1 - Solved
+# Hurdle 2 - Solved
+# Hurdle 3 - Solved
+# Hurdle 4 - Solved
+
+# Final project
+# maze problem - solved
